ec4243e984d083d404ba5fb99bca52741e735fc1
Stanimir <stanimir.k.stoyanov@gmail.com>
2017-11-01 17:40:04 +0200

    Add publisher handler

---
+++ includes/response/class-wordlift-dialogflow-get-publisher.php
@@ -0,0 +1,78 @@
+<?php
+
+/**
+ *
+ * @link       https://github.com/stoyan0v
+ * @since      1.0.0
+ *
+ * @package    Wordlift_For_Dialogflow
+ * @subpackage Wordlift_For_Dialogflow/response
+*/
+class Wordlift_For_Dialogflow_Get_Publisher extends Wordlift_For_Dialogflow_Response {
+
+	/**
+	 * The {@link Wordlift_Configuration_Service} instance.
+	 *
+	 * @since  1.0.0
+	 * @access private
+	 * @var \Wordlift_Configuration_Service $configuration_service The {@link Wordlift_Configuration_Service} instance.
+	 */
+	private $configuration_service;
+
+	public function __construct( $request ) {
+		parent::__construct( $request );
+
+		// Set the SPARQL service.
+		$this->set_configuration_service();
+	}
+
+	/**
+	 * Return the response.
+	 *
+	 * @access public
+	 * @abstract
+	 */
+	public function get_response() {
+		$publisher = $this->get_publisher();
+
+		// Return error message if the publisher is not configured.
+		if ( empty( $publisher ) ) {
+			return 'Sorry, the publisher has not yet being configured on this website';
+		}
+		
+		// Build the response adding prompt.
+		$response = $publisher->post_title . 'is the publisher of this website. Would you like to know more about them?';
+
+		// Check if we should display the full publisher info.
+		if ( ! empty( $this->get_param( 'full-info' ) ) ) {
+			$response = $publisher->post_content;
+		}
+
+		return $response;
+	}
+
+	/**
+	 * Setup configuration service
+	 *
+	 * @access public
+	 * @abstract
+	 */
+	private function set_configuration_service() {
+		$this->configuration_service = new Wordlift_Configuration_Service();
+	}
+
+	/**
+	 * Retrive the website publisher info.
+	 *
+	 * @return array Publisher post object or empty array if publisher is not found
+	 */
+	private function get_publisher() {
+		// Get publisher ID.
+		$publisher_id = $this->configuration_service->get_publisher_id();
+
+		// Get publisher
+		$publisher = get_post( $publisher_id );
+
+		return ( ! empty( $publisher ) ) ? $publisher : array() ;
+	}
+}
